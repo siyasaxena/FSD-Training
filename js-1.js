c8ead91690738f17f34dea014b65547573d905fb
siyasaxena <saxenasiya9@gmail.com>
2026-02-04 15:54:27 +0530

    basics of js

--- js-1.js
+++ js-1.js
@@ -2,3 +2,44 @@ console.log("Hello world");
 console.log(3 + 4);
 console.error("j");
 console.warn("k");
+
+var a = 10;
+let b = 20;
+const c = 30;
+let bool = true;
+let und;
+let nul = null;
+console.log(typeof bool);
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x % y);
+
+let age = 20;
+if (age >= 18) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+let arr = [10, 20, 30, 40, 50]; //array
+console.log(arr[0]);
+arr.push[60];
+arr.push[70];
+arr.pop();
+console.log(arr);
+
+//object
+let students = {
+  name: "varun",
+  age: 27,
+};
+
+//function
+function add(p, q) {
+  return p + q;
+}
+console.log(add(5, 7));
